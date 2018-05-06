681a9eee96be6340ab1e451f71c1c458f33b2632
neogeoss <neogeo-s@hanmail.net>
2018-05-06 21:27:35 +0900

    Random colors

--- colorGame.js
+++ colorGame.js
@@ -5,8 +5,9 @@ var colors = [
     "rgb(0, 255, 255)",
     "rgb(0, 0, 255)",
     "rgb(255, 0, 255)",
-
 ]
+
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
@@ -41,4 +42,22 @@ function changeColors(color) {
 function pickColor() {
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+
+function generateRandomColors(num) {
+    var arr = [];
+
+    //repeat num times
+    for(var i = 0; i < num; i++){
+        arr.push(randomColor());
+        //get random color and push into arr
+    }
+    return arr;
+}
+
+function randomColor() {
+    var r = Math.floor(Math.random() *256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
