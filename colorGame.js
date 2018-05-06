7add162fb9495a51462d05cb63ce9821bcf0603f
neogeoss <neogeo-s@hanmail.net>
2018-05-06 22:45:58 +0900

    Reset button defined

--- colorGame.js
+++ colorGame.js
@@ -14,6 +14,17 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function(){
+    colors = generateRandomColors(6);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = colors[i];
+    }
+});
 
 colorDisplay.textContent = pickedColor;
 
